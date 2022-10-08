6eee609b6e19110186848badb1da76f5215b700b
jkasari <jkasari@outlook.com>
2022-10-07 22:03:38 -0700

    forgot I was in the master branch

--- PLANT.ino
+++ PLANT.ino
@@ -5,3 +5,20 @@
 #define FLOWER_TWO_LEDS 3
 #define FLOWER_ONE_PIN 6
 #define FLOWER_TWO_PIN 7
+#define LED_TYPE WS2811
+#define COLOR_ORDER RGB
+
+CRGB flowerOne[FLOWER_ONE_LEDS];
+CRGB flowerTwo[FLOWER_TWO_LEDS];
+
+void setup() {
+  FastLED.addLeds<LED_TYPE, FLOWER_ONE_PIN, COLOR_ORDER>(flowerOne, FLOWER_ONE_LEDS);
+  FastLED.addLeds<LED_TYPE, FLOWER_TWO_PIN, COLOR_ORDER>(flowerTwo, FLOWER_TWO_LEDS);
+  FastLED.clear();
+}
+
+void loop() {
+  flowerOne[0] = CRGB::Red;
+  flowerTwo[0] = CRGB::Red;
+  FastLED.show();
+}
--- leaf.h
+++ leaf.h
@@ -1,7 +1,2 @@
 #include "leaf.cpp"
-#include <FastLED.h>
 
-#define FLOWER_ONE_LEDS 5
-#define FLOWER_TWO_LEDS 3
-#define FLOWER_ONE_PIN 6
-#define FLOWER_TWO_PIN 7
