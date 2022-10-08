744290edff0f6c76bfe5136fef50847cd38d57a0
jkasari <jkasari@outlook.com>
2022-10-07 22:55:37 -0700

    All the leds currently blink on and off with a sine wave

---
+++ Petal.cpp
@@ -0,0 +1,16 @@
+#include "Petal.h"
+#include <FastLED.h>
+
+
+CRGB Petal::getColor(void) {
+    uint8_t rgbArr[3];
+    for (int i = 0; i < 3; ++i) {
+        countArr[i] += 1;
+        rgbArr[i] = equate(countArr[i]);
+    }
+    return CRGB(rgbArr[0], rgbArr[1], rgbArr[2]);
+}
+
+uint8_t Petal::equate(uint32_t count) {
+    return triwave8(count);
+}
